7689daa4fcffe0b7e91d7d496bb8af9409ab317e
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-01 17:32:16 +0500

    Add files via upload

--- Day2&3Assignment/expressbackened/models/user.js
+++ Day2&3Assignment/expressbackened/models/user.js
@@ -1,1 +1,15 @@
+import mongoose from 'mongoose'
 
+//Defining schema
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String }
+    //username 
+
+
+})
+
+//Model 
+const userModel = mongoose.model("user", UserSchema)
+
+export default userModel;
