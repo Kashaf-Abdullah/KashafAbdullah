047e2f30a00a8d52a6813643416a60f58c8391c6
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-01 17:30:39 +0500

    Add files via upload

--- Day2&3Assignment/expressbackened/routes/web.js
+++ Day2&3Assignment/expressbackened/routes/web.js
@@ -1,1 +1,8 @@
+import express from 'express';
+import UserController from '../controllers/userController.js';
 
+const router = express.Router();
+
+router.get('/user', UserController.getAllDoc)
+
+export default router
