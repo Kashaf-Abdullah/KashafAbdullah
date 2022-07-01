8944d526001f98fb340b5f709d555478aed4da09
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-01 17:34:08 +0500

    Add files via upload

--- Day2&3Assignment/expressbackened/db/connectdb.js
+++ Day2&3Assignment/expressbackened/db/connectdb.js
@@ -1,1 +1,16 @@
+import mongoose from 'mongoose'
 
+const connectDB = async (DATABASE_URL) => {
+    try {
+        const DB_OPTIONS = {
+            dbName: 'webstarHW',
+        }
+        await mongoose.connect(DATABASE_URL, DB_OPTIONS);
+        console.log('Connected Successfully');
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+export default connectDB;
