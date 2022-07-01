d8faf2c2140bd2c3c3ba51d5e53035df85b7af25
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-01 17:28:00 +0500

    Add files via upload

--- Day2&3Assignment/expressbackened/controllers/userController.js
+++ Day2&3Assignment/expressbackened/controllers/userController.js
@@ -1,1 +1,15 @@
+import userModel from "../models/user.js";
+class userController {
+    static getAllDoc = async (req, res) => {
+        try {
+            const result = await userModel.find()
+            res.send(result);
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
 
+}
+
+export default userController;
