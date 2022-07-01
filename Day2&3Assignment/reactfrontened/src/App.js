cb192837bf17c9fd45d3e25a533e9c1ea7393a2f
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-01 17:38:54 +0500

    Add files via upload

--- Day2&3Assignment/reactfrontened/src/App.js
+++ Day2&3Assignment/reactfrontened/src/App.js
@@ -1,1 +1,40 @@
 
+import './App.css';
+import { useState, useEffect } from "react"
+import axios from 'axios'
+function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    async function getAllUsernames() {
+      try {
+        const users = await axios.get("http://localhost:4000/api/user")
+        console.log(users.data)
+        setUsers(users.data);
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+    getAllUsernames()
+  }, [])
+  return (
+    <>
+      <h1>Kashaf Abdullah</h1>
+      <h4> WEBSTAR ROADMAP TO MERNSTACK ASSIGNMENT</h4>
+      {
+        users.map((user, i) => {
+          return (
+            //all names in list order
+            <ul>
+              <li >Name:{user.name}<br></br></li>
+            </ul>
+          )
+        })
+      }
+
+    </>
+  );
+}
+
+export default App;
